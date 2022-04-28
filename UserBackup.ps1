fef697e603d79cd97860349aacb8b308f9747281
MitchX <mehricke99@gmail.com>
2022-04-28 13:18:02 -0500

    V1.0
    - Backup Basic User Directories
    - Log System Information
    - Log Files Backed up and Restored

---
+++ UserBackup.ps1
@@ -0,0 +1,50 @@
+# Data Backup Directory
+$backup_destination = "E:\UserBackup\" + $env:COMPUTERNAME + "_" + $date
+
+#Log File Directory
+$log_file = $backup_destination + "\backup.log"
+
+$date = Get-Date -Format "MM.dd.yyyy"
+$time = Get-Date -UFormat "%R"
+
+#List of directories to back up
+$backup_directories = ("$env:USERPROFILE\Contacts",
+                      "$env:USERPROFILE\Desktop",
+                      "$env:USERPROFILE\Documents",
+                      "$env:USERPROFILE\Downloads",
+                      "$env:USERPROFILE\AppData\Roaming\Microsoft\Signatures",
+                      "$env:USERPROFILE\Pictures",
+                      "$env:USERPROFILE\Videos",
+                      "C:\Users\Public\Caterpillar",
+                      "$env:USERPROFILE\AppData\Local\Google\Chrome\User Data\Default\Bookmarks",
+                      "$env:USERPROFILE\AppData\Local\Google\Chrome\User Data\Default\Bookmarks.bak"
+ 
+                      )  
+
+#Function to output to backup.log
+Function LogWrite
+{
+   Param ([string]$logstring)
+
+   Add-content $log_file -value $logstring
+}
+
+#Appends Important Data to Log File
+LogWrite "-------+ Backup Information +-------"
+LogWrite "ComputerName: $env:COMPUTERNAME"
+LogWrite "Username: $env:USERNAME"
+LogWrite "Computer Model: $env:Model"
+LogWrite "OS Version: $([System.Environment]::OSVersion.VersionString)"
+LogWrite "Serial Number: $env:Serial"
+LogWrite "Backed up at: $time"
+LogWrite "----------------------------------`r`n"
+
+
+# Copies Directories to Backup Folder
+foreach ($dir in $backup_directories)
+{
+   Write-Host -ForegroundColor Cyan "Copying $dir"
+   Copy-Item -Path $dir -Destination $backup_destination -Force -Recurse
+   LogWrite "Copied $dir"
+}
+Write-Host -ForegroundColor green "`r`nBackup Complete, Log file saved to $log_file"
---
+++ UserRestore.ps1
@@ -0,0 +1,65 @@
+# Location of Backed up Data
+$restore_directory = "E:\UserBackup\" + $env:COMPUTERNAME + "_" + $date
+
+#Log File Directory
+$log_file = $restore_directory + "\restore.log"
+
+$date = Get-Date -Format "MM.dd.yyyy"
+$time = Get-Date -UFormat "%R"
+
+#Directories to restore backup files to
+$restore_destination = ("$env:USERPROFILE\Contacts",
+                      "$env:USERPROFILE\Desktop",
+                      "$env:USERPROFILE\Documents",
+                      "$env:USERPROFILE\Downloads",
+                      "$env:USERPROFILE\AppData\Roaming\Microsoft\Signatures",
+                      "$env:USERPROFILE\Pictures",
+                      "$env:USERPROFILE\Videos",
+                      "C:\Users\Public\Caterpillar",
+                      "$env:USERPROFILE\AppData\Local\Google\Chrome\User Data\Default\Bookmarks",
+                      "$env:USERPROFILE\AppData\Local\Google\Chrome\User Data\Default\Bookmarks.bak"
+ 
+                      )
+
+#Directories of backed up files
+$restore_origin = ("$restore_directory\Contacts\*",
+                    "$restore_directory\Desktop\*",
+                    "$restore_directory\Documents\*",
+                    "$restore_directory\Downloads\*",
+                    "$restore_directory\Signatures\*",
+                    "$restore_directory\Pictures\*",
+                    "$restore_directory\Videos\*",
+                    "$restore_directory\Caterpillar\*",
+                    "$restore_directory\Bookmarks",
+                    "$restore_directory\Bookmarks.bak"
+                    )
+
+#Function to output to restore.log
+Function LogWrite
+{
+   Param ([string]$logstring)
+
+   Add-content $log_file -value $logstring
+}
+
+#Appends Important Data to Log File
+LogWrite "-------+ Restore Information +-------"
+LogWrite "ComputerName: $env:COMPUTERNAME"
+LogWrite "Username: $env:USERNAME"
+LogWrite "Computer Model: $env:Model"
+LogWrite "OS Version: $([System.Environment]::OSVersion.VersionString)"
+LogWrite "Serial Number: $env:Serial"
+LogWrite "Restored at: $time on $date"
+LogWrite "----------------------------------`r`n"
+
+$i = 0
+# Copies Directories to Backup Folder
+foreach ($dir in $restore_origin)
+{
+   $dest = $restore_destination[$i].ToString()
+   Write-Host -ForegroundColor Cyan "`r`nCopying $dir to $dest"
+   Copy-Item -Path $dir -Destination $dest -Force -Recurse
+   LogWrite "Restored $dir to $dest"
+   $i++
+}
+Write-Host -ForegroundColor green "`r`nRestore Complete, Log file saved to $log_file"
